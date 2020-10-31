5ee52f7e9759b1ebc79ac21d7bee959b2739de0a
Nguyen Nguyen <nltn@msn.com>
2020-10-31 14:55:59 -0700

    Fixed a bug in the function mod(int n1, int n2)

--- DiscreteMathHelper/helper.h
+++ DiscreteMathHelper/helper.h
@@ -23,7 +23,15 @@ public:
 
 	static int mod(int n1, int n2)
 	{
-		return n1 < 0 ? n1 % n2 + n2 : n1 % n2;
+        // return n1 < 0 ? n1 % n2 + n2 : n1 % n2;
+        if(n1 < 0)
+        {
+            return n1 % n2 == 0 ? n1 % n2 : n1 % n2 + n2;
+        }
+        else
+        {
+            return n1 % n2;
+        }
 	}
 
 	template <typename T>
