df91f8d0ec071bc90433278ed113916cb434390c
Nguyen Nguyen <nltn@msn.com>
2020-11-02 14:35:11 -0800

    Deleted the private mod() function.

--- DiscreteMathHelper/symmetric_key_crypto.h
+++ DiscreteMathHelper/symmetric_key_crypto.h
@@ -7,6 +7,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "helper.h"
 
 class SymmetricKeyCrypto
 {
@@ -16,9 +17,6 @@ public:
 
 	int encrypt(int m, int key, int N);
 	int decrypt(int c, int key, int N);
-
-private:
-	int mod(int n1, int n2);
 };
 
 int SymmetricKeyCrypto::encrypt(int m, int key, int N)
@@ -28,10 +26,5 @@ int SymmetricKeyCrypto::encrypt(int m, int key, int N)
 
 int SymmetricKeyCrypto::decrypt(int c, int key, int N)
 {
-	return mod(c - key, N);
-}
-
-int SymmetricKeyCrypto::mod(int n1, int n2)
-{
-	return n1 < 0 ? n1 % n2 + n2 : n1 % n2;
+	return Helper::mod(c - key, N);
 }
