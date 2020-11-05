16907ee0c62ba682a8072506fb5956791e692823
Nguyen Nguyen <nltn@msn.com>
2020-11-04 18:59:22 -0800

    Updated Text Encryption and Decryption

--- DiscreteMathHelper/functions.h
+++ DiscreteMathHelper/functions.h
@@ -183,70 +183,174 @@ void func_SimpleDecrypt()
 
 void func_RSA_Encryption()
 {
-	int p, q, e, m;
-	Helper::prompt("Enter a prime number p: ", p);
-	Helper::prompt("Enter a prime number q: ", q);
-	Helper::prompt("Enter a prime number for public key e: ", e);
-	RSA rsa(p, q, e);
-	Helper::prompt("Enter message as Integer m: ", m);
-	cout  << endl  << "Encryption formula: "
+    int p, q, e, m;
+    Helper::prompt("Enter a prime number p: ", p);
+    Helper::prompt("Enter a prime number q: ", q);
+    Helper::prompt("Enter a prime number for public key e: ", e);
+    
+    RSA rsa(p, q, e);
+    int phi = (p - 1) * (q - 1);
+    cout << "\n  N = p * q = " << p * q << endl;
+    cout << "  Phi = (p - 1) * (q - 1) = " << (p - 1) * (q - 1) << endl;
+
+    EuclideanAlgorithm algo;
+    auto gcdResult = algo.gcdExtended(e, phi);
+    cout << "  gcdExtended(" << e << ", " << phi << ") = " << gcdResult << " = " << gcdResult.gcd << endl;
+    cout << "  Private key d = " << gcdResult.s << " mod " << phi << " = " << rsa.getPrivateKey() << endl;
+    cout << endl;
+    
+    Helper::prompt("Enter message as Integer m: ", m);
+    cout  << endl  << "Encryption formula: "
         << rsa.getEncryptionFormula(m) << endl;
 }
 
 void func_RSA_EncryptText()
 {
-	int p, q, e, bits;
-	string m;
-	Helper::prompt("Enter a prime number p: ", p);
-	Helper::prompt("Enter a prime number q: ", q);
-	Helper::prompt("Enter a prime number for public key e: ", e);
+    int p, q, e, bits;
+    string m;
+    Helper::prompt("Enter a prime number p: ", p);
+    Helper::prompt("Enter a prime number q: ", q);
+    Helper::prompt("Enter a prime number for public key e: ", e);
+    
+    RSA rsa(p, q, e);
+    int phi = (p - 1) * (q - 1);
+    cout << "\n  N = p * q = " << p * q << endl;
+    cout << "  Phi = (p - 1) * (q - 1) = " << (p - 1) * (q - 1) << endl;
 
-	RSA rsa(p, q, e);
-	Helper::prompt("Enter plain text message: ", m);
-	Helper::prompt("Enter the number of bits per char: ", bits);
-	NumericalMessage nMsg;
-	Binary temp = nMsg.textToBinary(m, bits);
-	Binary temp2("1");
-	cout << endl  << "Encryption formula: "
-        << rsa.getEncryptionFormula((temp + temp2).toInteger()) << endl;
+    EuclideanAlgorithm algo;
+    auto gcdResult = algo.gcdExtended(e, phi);
+    cout << "  gcdExtended(" << e << ", " << phi << ") = " << gcdResult << " = " << gcdResult.gcd << endl;
+    cout << "  Private key d = " << gcdResult.s << " mod " << phi << " = " << rsa.getPrivateKey() << endl;
+    cout << endl;
+
+    Helper::prompt("Enter plain text message: ", m);
+    Helper::prompt("Enter the number of bits per char: ", bits);
+    NumericalMessage nMsg;
+    Binary temp = nMsg.textToBinary(m, bits);
+    
+    // =========== STRING TO INT CONVERTION SCHEME - (BEGIN) =============
+    cout << "\n========= String to Integer Convertion Scheme =========\n";
+    cout << "1. Replace each letter with its position in the alphabet" << endl;
+    cout << "   and Turn each number into a 4-bit binary number" << endl;
+
+    for (int i = 0; i < m.length(); i++)
+    {
+        int num = nMsg.charToNumber(m[i]);
+        string bin = Converter::fromDec(num, 2);
+        bin.insert(0, std::string(bits - bin.length(), '0'));
+        
+        cout << m[i] << " = " << num << " = " << bin << endl;
+    }
+    cout << endl;
+    
+    cout << "2. Concatenate the bits" << endl;
+    cout << temp << endl << endl;
+
+    cout << "3. Add a number (In the lecture slides, she adds 1)" << endl;
+    int shift;
+    Helper::prompt("Enter an Interger number you want to add: ", shift);
+    Binary temp2(shift);
+    
+    cout << "Convert " << shift << " to binary: "
+        << temp2.toString().insert(0, std::string(bits - temp2.toString().length(), '0'))
+        << endl << endl;
+    cout << temp.toString() << " + "
+        << temp2.toString().insert(0, std::string(bits - temp2.toString().length(), '0'))
+        << " = " << (temp + temp2).toString() << " = " << (temp + temp2).toInteger()
+        << endl;
+    // ========= STRING TO INT CONVERTION SCHEME - (END) ===============
+    
+    cout << endl << string(50, '*') << endl;
+    cout << "Encryption formula: "
+        << rsa.getEncryptionFormula((temp + temp2).toInteger());
+    cout << endl << string(50, '*') << endl;
 }
 
 void func_RSA_Decryption()
 {
-	int p, q, e, c;
-	Helper::prompt("Enter a prime number p: ", p);
-	Helper::prompt("Enter a prime number q: ", q);
-	Helper::prompt("Enter a prime number for public key e: ", e);
-	RSA rsa(p, q, e);
+    int p, q, e, c;
+    Helper::prompt("Enter a prime number p: ", p);
+    Helper::prompt("Enter a prime number q: ", q);
+    Helper::prompt("Enter a prime number for public key e: ", e);
+    
+    RSA rsa(p, q, e);
+    int phi = (p - 1) * (q - 1);
+    cout << "\n  N = p * q = " << p * q << endl;
+    cout << "  Phi = (p - 1) * (q - 1) = " << (p - 1) * (q - 1) << endl;
+    
+    EuclideanAlgorithm algo;
+    auto gcdResult = algo.gcdExtended(e, phi);
+    cout << "  gcdExtended(" << e << ", " << phi << ") = " << gcdResult << " = " << gcdResult.gcd << endl;
+    cout << "  Private key d = " << gcdResult.s << " mod " << phi << " = " << rsa.getPrivateKey() << endl;
+    cout << endl;
+
     Helper::prompt("Enter the cipher as Interger c: ", c);
-    cout << endl << "Private key: d  = " << rsa.getPrivateKey() << endl;
-	cout << "Decryption formula: " << rsa.getDecryptionFormula(c) << endl;
+    cout << "Decryption formula: " << rsa.getDecryptionFormula(c) << endl;
 }
 
 void func_RSA_DecryptText()
 {
-	int p, q, e, c;
-	Helper::prompt("Enter a prime number p: ", p);
-	Helper::prompt("Enter a prime number q: ", q);
-	Helper::prompt("Enter a prime number for public key e: ", e);
-	RSA rsa(p, q, e);
+    int p, q, e, c;
+    Helper::prompt("Enter a prime number p: ", p);
+    Helper::prompt("Enter a prime number q: ", q);
+    Helper::prompt("Enter a prime number for public key e: ", e);
+
+    RSA rsa(p, q, e);
+    int phi = (p - 1) * (q - 1);
+    cout << "\n  N = p * q = " << p * q << endl;
+    cout << "  Phi = (p - 1) * (q - 1) = " << (p - 1) * (q - 1) << endl;
+    EuclideanAlgorithm algo;
+    auto gcdResult = algo.gcdExtended(e, phi);
+    cout << "  gcdExtended(" << e << ", " << phi << ") = " << gcdResult << " = " << gcdResult.gcd << endl;
+    cout << "  Private key d = " << gcdResult.s << " mod " << phi << " = " << rsa.getPrivateKey() << endl;
+    cout << endl;
+
     Helper::prompt("Enter the cipher as Interger c: ", c);
-    cout << endl << "Private key: d = " << rsa.getPrivateKey() << endl;
-	cout << "Decryption formula: " << rsa.getDecryptionFormula(c) << endl;
+    cout << endl << string(50, '*') << endl;
+    cout << "Decryption formula: " << rsa.getDecryptionFormula(c);
+    cout << endl << string(50, '*') << endl;
 
-	int result, bits;
-	Helper::prompt("Enter the result of m: ", result);
-	Helper::prompt("Enter the number of bits per char: ", bits);
-	Binary bin(result);
-	Binary temp("1");
-	NumericalMessage nMsg;
-	cout << nMsg.binaryToText((bin - temp).toString(), bits);
-}
+    int result, bits;
+    Helper::prompt("Enter the result of m: ", result);
+    Helper::prompt("Enter the number of bits per char: ", bits);
 
-void func_PrintRSAFormulas()
-{
-	cout << "N = p * q" << endl;
-	cout << "phi = (p - 1) * (q - 1)" << endl;
-	cout << "Encrypt formula: c = m^e mod N " << endl;
-	cout << "Decrypt formula: m = c^d mod N " << endl;
+    Binary bin(result);
+    cout << "\n1. Convert the number " << result << " to Binary:" << endl;
+    cout << result << " = " << bin.toString() << endl << endl;
+
+    int shift;
+    cout << "2. Subtract a number (If 1 is added in the Encrytion, then subtract 1)" << endl;
+    Helper::prompt("Enter an Interger number you want to subtract: ", shift);
+
+    Binary temp2(shift);
+    string tranformedBinStr = (bin - temp2).toString();
+    if (!(tranformedBinStr.length() / 4))
+    {
+        tranformedBinStr = tranformedBinStr.insert(0, std::string(bits - tranformedBinStr.length() % bits, '0'));
+    }
+
+    cout << "\nConvert " << shift << " to binary: "
+        << temp2.toString().insert(0, std::string(bits - temp2.toString().length(), '0'))
+        << endl << endl;
+
+    cout << bin.toString().insert(0, std::string(bits - bin.toString().length() % bits, '0'))
+        << " - "
+        << temp2.toString().insert(0, std::string(bits - temp2.toString().length(), '0'))
+        << " = "
+        << tranformedBinStr
+        << endl;
+
+    cout << "\n======= Integer to String Convertion Scheme =======" << endl;
+    NumericalMessage nMsg;
+    for (int i = 0; i < tranformedBinStr.length(); i += bits)
+    {
+        string seg = tranformedBinStr.substr(i, bits);
+        cout << seg << " = " << Converter::binaryStringToDec(seg)
+            << " = " << nMsg.numberToChar(Converter::binaryStringToDec(seg))
+            << endl;
+    }
+
+    cout << "\n****************** Concatinate *******************" << endl;
+    cout << nMsg.binaryToText((bin - temp2).toString(), bits);
+    cout << endl << string(50, '*') << endl;
 }
