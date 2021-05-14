bf9f0e0a2522cda5b487406bc9de822bdf95f380
Nguyen Nguyen <nltn@msn.com>
2021-05-13 23:28:36 -0700

    Improved the efficiency of isPrime() function

--- DiscreteMathHelper/prime_number.h
+++ DiscreteMathHelper/prime_number.h
@@ -12,43 +12,43 @@
 class PrimeFactorizationResult
 {
 public:
-    PrimeFactorizationResult() { };
-    PrimeFactorizationResult(const PrimeFactorizationResult& other) : factors(other.factors) { };
-    
-    void add(size_t factor)
-    {
-        auto it = factors.find(factor);
-        if (it != factors.end())
-            ++(it->second);
-        else
-        {
-            factors.insert(std::make_pair(factor, 1));
-        }
-    }
-        
-    friend std::ostream& operator<<(std::ostream& os, const PrimeFactorizationResult& rhs)
-    {
-        for (auto it = rhs.factors.cbegin(); it != rhs.factors.cend(); it++)
-        {
-            os << '(' << it->first << '^' << it->second << ") ";
-        }
-        return os;
-    }
+	PrimeFactorizationResult() { };
+	PrimeFactorizationResult(const PrimeFactorizationResult& other) : factors(other.factors) { };
+	
+	void add(size_t factor)
+	{
+		auto it = factors.find(factor);
+		if (it != factors.end())
+			++(it->second);
+		else
+		{
+			factors.insert(std::make_pair(factor, 1));
+		}
+	}
+		
+	friend std::ostream& operator<<(std::ostream& os, const PrimeFactorizationResult& rhs)
+	{
+		for (auto it = rhs.factors.cbegin(); it != rhs.factors.cend(); it++)
+		{
+			os << '(' << it->first << '^' << it->second << ") ";
+		}
+		return os;
+	}
 private:
-    std::map<size_t, int> factors;
+	std::map<size_t, int> factors;
 };
 
 class PrimeNumber
 {
 public:
-    PrimeNumber() = delete;
+	PrimeNumber() = delete;
 
 	static size_t nextPrime(size_t N)
 	{
-        size_t prime = N + 1;
-        
+		size_t prime = N + 1;
+		
 		while (!isPrime(prime))
-        {
+		{
 			++prime;
 		}
 
@@ -57,10 +57,10 @@ public:
 
 	static bool isPrime(size_t num)
 	{
-        if (num == 2)
-        {
-            return true;
-        }
+		if (num == 2)
+		{
+			return true;
+		}
 		else if (num < 2 || num % 2 == 0)
 		{
 			return false;
@@ -79,32 +79,32 @@ public:
 		}
 	};
 
-    static PrimeFactorizationResult factorize(size_t num)
-    {
-        PrimeFactorizationResult result;
-        
-        // Although the number 1 is not a prime nor a composite number,
-        // The function still returns 1 as a factor.
-        if (num < 2)
-        {
-            //std::cerr << "This is not a prime nor a composite number" << std::endl;
-            result.add(num);
-            return result;
-        }
+	static PrimeFactorizationResult factorize(size_t num)
+	{
+		PrimeFactorizationResult result;
+		
+		// Although the number 1 is not a prime nor a composite number,
+		// The function still returns 1 as a factor.
+		if (num < 2)
+		{
+			//std::cerr << "This is not a prime nor a composite number" << std::endl;
+			result.add(num);
+			return result;
+		}
 
-        size_t prime = 2;
-        while (prime <= num)
-        {
-            if (num % prime == 0)
-            {
-                result.add(prime);
-                num /= prime;
-            }
-            else
-            {
-                prime = nextPrime(prime);
-            }
-        }
-        return result;
-    }
+		size_t prime = 2;
+		while (prime <= num)
+		{
+			if (num % prime == 0)
+			{
+				result.add(prime);
+				num /= prime;
+			}
+			else
+			{
+				prime = nextPrime(prime);
+			}
+		}
+		return result;
+	}
 };
